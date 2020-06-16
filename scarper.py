b53dc8d71a2a9f31e2376c9e2ff1022534bd18ce
szymon-scrape <67016403+szymon-scrape@users.noreply.github.com>
2020-06-16 18:51:25 +0200

    Update scarper.py

--- scarper.py
+++ scarper.py
@@ -10,39 +10,32 @@ headers = {'User-Agent':
 '(KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 OPR/68.0.3618.142'}
 
 def gen():
-    nd_part = (''.join(choice(ascii_lowercase) for i in range(4) )) # generowanie 4 liter do linku
+    nd_part = (''.join(choice(ascii_lowercase) for i in range(4) )) # Generating 4 characters for first part of link
 
-    nd_part += (''.join(choice(digits) for i in range(2) )) # generowanie 2 cyfr do linku
+    nd_part += (''.join(choice(digits) for i in range(2) )) # Generating 2 digits for last part of link
 
-    url = 'https://prnt.sc/' + nd_part # tworzenie działającego linku
+    url = 'https://prnt.sc/' + nd_part # Assembling into working link
 
 
 
     global page
     page = requests.get(url, headers=headers)
 
-    #print(url)
+list = [] # Empty list - will be used as storage for URL's
 
-
-
-#print(soup)
-list = [] # pusta lista - magazyn na url z obrazkami
-
-for i in range(111): # range(liczba linkow, ktore maja zostac wygenerowane)
+for i in range(111): # range(Amount of links you want to generate)
     gen()
     soup = BeautifulSoup(page.content, 'html.parser')
     for img in soup.findAll('img'):
         temp = img.get('src')
         if 'image.' in temp:
             list.append(temp)
-#print(list)
-counter = '0'
+counter = '0' # Counter is a string because I will use it in filename, then change type to int -> increment -> change to string again
 
 for i in list:
-    filename = 'zdjęcie' + counter + '.jpg'
+    filename = 'photo' + counter + '.jpg'
     tmp = int(counter)
     tmp += 1
     counter = str(tmp)
-    #print(i)
     r = requests.get(i, allow_redirects = True)
     open(filename, 'wb').write(r.content)
